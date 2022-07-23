91b1690a6426d0a61ed5c48771af3df68baab97b
Daniel Carvalho <daniel.devop@gmail.com>
2022-07-23 17:14:15 -0300

    buscando dados do banco

--- src/main/java/dev/unscrud/imersaojava/LinguagemController.java
+++ src/main/java/dev/unscrud/imersaojava/LinguagemController.java
@@ -2,22 +2,19 @@ package dev.unscrud.imersaojava;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LinguagemController {
 
-  private List<Linguagem> linguagens = List.of(
-      new Linguagem("Java",
-          "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_256x256.png",
-          1),
-      new Linguagem("PHP",
-          "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/php/php_256x256.png",
-          2));
+  @Autowired
+  private LinguagemRepository repository;
 
   @GetMapping("/linguagens")
   public List<Linguagem> obterLinguagens() {
+    List<Linguagem> linguagens = repository.findAll();
     return linguagens;
   }
 }
