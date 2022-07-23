5fd6ac73f1786922847ba2942939159c8204d6b4
Daniel Carvalho <daniel.devop@gmail.com>
2022-07-23 14:54:21 -0300

    initial commit

---
+++ src/main/java/dev/unscrud/imersaojava/ImersaoJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.unscrud.imersaojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImersaoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImersaoJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/unscrud/imersaojava/ImersaoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.unscrud.imersaojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImersaoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
