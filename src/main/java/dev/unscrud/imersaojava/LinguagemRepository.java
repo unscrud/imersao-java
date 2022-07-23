e6b3d89e2f5af156ae0626eded749e7b6486994c
Daniel Carvalho <daniel.devop@gmail.com>
2022-07-23 17:14:55 -0300

    interface de acesso a dados

---
+++ src/main/java/dev/unscrud/imersaojava/LinguagemRepository.java
@@ -0,0 +1,6 @@
+package dev.unscrud.imersaojava;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface LinguagemRepository extends MongoRepository<Linguagem, String> {
+}
