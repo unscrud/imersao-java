cf9c58aa4b29cde4f0a1cde7665025f9e0398d44
Daniel Carvalho <daniel.devop@gmail.com>
2022-07-23 23:00:03 -0300

    salvando uma linguagem

--- src/main/java/dev/unscrud/imersaojava/LinguagemController.java
+++ src/main/java/dev/unscrud/imersaojava/LinguagemController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +18,11 @@ public class LinguagemController {
   public List<Linguagem> obterLinguagens() {
     List<Linguagem> linguagens = repository.findAll();
     return linguagens;
+  }
+
+  @PostMapping("/linguagens")
+  public Linguagem salvarLinguagem(@RequestBody Linguagem linguagem) {
+    Linguagem linguagemSalva = repository.save(linguagem);
+    return linguagemSalva;
   }
 }
